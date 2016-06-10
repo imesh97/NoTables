2ea0185005652666403b799472bdf00bcf49d076
imesh97 <imesh.nimsitha@gmail.com>
2016-06-09 22:53:39 -0400

    Made the main class.

---
+++ src/main/java/xyz/imdafatboss/notables/Home.java
@@ -0,0 +1,15 @@
+package xyz.imdafatboss.notables;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Home extends JavaPlugin implements Listener{
+
+    @Override
+    public void onEnable(){
+
+        this.getLogger().info("Created by imdafatboss.");
+
+    }
+
+}
