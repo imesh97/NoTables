c49e3b2abfbcbe0edaaac0fa06e0703af376c93c
imesh97 <imesh.nimsitha@gmail.com>
2016-06-09 23:14:57 -0400

    Initialized the FileManager.

--- src/main/java/xyz/imdafatboss/notables/Home.java
+++ src/main/java/xyz/imdafatboss/notables/Home.java
@@ -2,13 +2,17 @@ package xyz.imdafatboss.notables;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.notables.config.FileManager;
 
 public class Home extends JavaPlugin implements Listener{
+
+    FileManager fm = new FileManager(this);
 
     @Override
     public void onEnable(){
 
         this.getLogger().info("Created by imdafatboss.");
+        fm.getConfig("config.yml").saveDefaultConfig();
 
     }
 
